dbabcb5d52ba1f60a50c23e8237e1c1140c8c96f
Joao Luiz <joaoluizmarcelinoribeiro@gmail.com>
2023-10-02 11:42:34 -0300

    Initial Commit

---
+++ signosAppStudy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  signosAppStudy
+//
+//  Created by Joao Luiz Marcelino Ribeiro on 02/10/23.
+//  Copyright © 2023 Curso IOS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
